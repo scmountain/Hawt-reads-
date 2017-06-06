e00fc19f03d3f278fda06145ac49ee381693e82c
Spencer Carter <scmountain@gmail.com>
2017-06-05 19:48:43 -0600

    lists the top 10 hawts

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,13 @@
 class Link < ActiveRecord::Base
   validates :url, presence: true
-  validates :views, presence: true
+
+  def self.top_ten
+    require "pry"; binding.pry
+    all.order('views DESC')
+    .limit(10)
+  end
+
+  def is_hot
+    top_ten[0]
+  end
 end
--- spec/model/link_spec.rb
+++ spec/model/link_spec.rb
@@ -3,6 +3,5 @@ require "rails_helper"
 RSpec.describe Link, type: :model do
   context "validations" do
     it {should validate_presence_of(:url)}
-    it {should validate_presence_of(:views)}
   end
 end
